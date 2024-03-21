137da33a47250420d06cd54c4721251d07cff1d9
Daniel Yates <145247690+Acid-base@users.noreply.github.com>
2024-03-21 14:52:48 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function sum(numbers) {
   return total;
 }
 console.log("Sum of [1, 2, 3, 4, 5]:", sum([1, 2, 3, 4, 5]));
-
+//
 function average(numbers) {
   if (numbers.length === 0) {
     return 0; // Return 0 if the array is empty to avoid division by zero
@@ -20,7 +20,7 @@ function average(numbers) {
   return total / numbers.length;
 }
 console.log("Average of [1, 2, 3, 4, 5]:", average([1, 2, 3, 4, 5]));
-
+//
 function findLongest(strings) {
   let longestString = strings[0]; // Initialize longestString with the first element
   for (let i = 1; i < strings.length; i++) {
@@ -32,7 +32,22 @@ function findLongest(strings) {
   return longestString;
 }
 console.log("Longest string in array:", findLongest(["red", "green", "blue"]));
-//function filterif(strings, length) {}
+//
+function filterIf(strings, length) {
+  const filteredStrings = [];
+  for (let i = 0; i < strings.length; i++) {
+    if (strings[i].length > length) {
+      filteredStrings.push(strings[i]);
+    }
+  }
+  return filteredStrings;
+}
+console.log(
+  "Strings over 5 char ['red', 'green', 'blue', 'orange']: ",
+  filterIf(["red", "green", "blue", "orange"], 5)
+);
+//
+
 //function print(n) {}
 //function sortage(data) {}
 //function filterover50(data) {}
