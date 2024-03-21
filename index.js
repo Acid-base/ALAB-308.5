0d419f26bc04f3c01caa0a90175cf5926519fe15
Daniel Yates <145247690+Acid-base@users.noreply.github.com>
2024-03-21 14:26:14 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-function sum(numbers) {}
-function average(numbers) {}
-function findlongest(strings) {}
-function filterif(strings, length) {}
-function print(n) {}
-function sortage(data) {}
-function filterover50(data) {}
-function maparray(data) {}
-function agesum(data) {}
-function ageavg(data) {}
-function incrementage(object) {}
-function agecopyreturn(object) {}
+function sum(numbers) {
+    let total = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        total += numbers[i];
+    }
+    return total;
+}
+console.log(sum([1, 2, 3, 4, 5, 6])
+//function average(numbers) {}
+//function findlongest(strings) {}
+//function filterif(strings, length) {}
+//function print(n) {}
+//function sortage(data) {}
+//function filterover50(data) {}
+//function maparray(data) {}
+//function agesum(data) {}
+//function ageavg(data) {}
+//function incrementage(object) {}
+//function agecopyreturn(object) {}
