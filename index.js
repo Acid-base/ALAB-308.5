e257e138edbe8e3dd9047098a3cfa8a7dfc59b62
Daniel Yates <145247690+Acid-base@users.noreply.github.com>
2024-03-21 14:15:20 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+function sum(numbers) {}
+function average(numbers) {}
+function findlongest(strings) {}
+function filterif(strings, length) {}
+function print(n) {}
+function sortage(data) {}
+function filterover50(data) {}
+function maparray(data) {}
+function agesum(data) {}
+function ageavg(data) {}
+function incrementage(object) {}
+function agecopyreturn(object) {}
